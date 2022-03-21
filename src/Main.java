c0634147ef65b0834909894bea13eda76da483a3
dunghojava <dungho21397@gmail.com>
2022-03-21 10:02:42 +0700

    variable data type

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 20.5f;
+        double c = 20.5f;
+        boolean d = true;
+        char e = 'e';
+        String f = "Hà Nội";
+        System.out.println("a= " + a);
+        System.out.println("b= " + b);
+        System.out.println("c= " + c);
+        System.out.println("d= " + d);
+        System.out.println("e= " + e);
+        System.out.println("f= " + f);
+    }
+}
